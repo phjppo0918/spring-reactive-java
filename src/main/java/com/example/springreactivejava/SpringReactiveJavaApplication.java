8c16ef7aef28dec115ff4e2c3ce06e6d34935982
Hyeonjun Park <phjppo0918@gmail.com>
2023-04-23 13:50:51 +0900

    feat : init setting

---
+++ src/main/java/com/example/springreactivejava/SpringReactiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreactivejava/SpringReactiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
